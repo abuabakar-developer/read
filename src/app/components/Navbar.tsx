4beee697d0e0275903712d562e5cc9b82f69d860
abuabakar-developer <157199734+abuabakar-developer@users.noreply.github.com>
2024-11-08 20:58:39 -0800

    commit change

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -118,11 +118,11 @@ const Navbar: React.FC<NavbarProps> = ({ onSearch, onCategorySelect }) => {
              
       {/* Upper Navbar - visible on larger screens */}
       <nav className="bg-gray-50 border-b border-green-300 fixed w-full top-0 z-50 shadow-md">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 xl:px-12">
+        <div className="mx-auto px-4 sm:px-6 lg:px-8 xl:px-12">
           <div className="flex justify-between items-center h-16">
             {/* Logo */}
             
-    <Link href="/" className="text-green-900 text-4xl font-bold tracking-wide ml-6 hover:text-green-500 font-cinzel">
+    <Link href="/" className="text-green-900 text-4xl font-bold tracking-wide ml-2 hover:text-green-500 font-cinzel">
     ABreads
     </Link>
             {/* Search Bar - visible on larger screens */}
@@ -203,8 +203,10 @@ const Navbar: React.FC<NavbarProps> = ({ onSearch, onCategorySelect }) => {
         </div>
       </nav>
 
+
+
       <nav className="fixed w-full top-16 z-50 bg-white border-t border-gray-300 shadow-lg">
-  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 xl:px-12 py-2 flex justify-between items-center">
+  <div className="mx-auto px-4 sm:px-6 lg:px-8 xl:px-12 py-2 flex justify-between items-center">
     {/* Categories */}
     <div className="flex items-center space-x-4">
       <div className="relative lg:hidden">
@@ -212,7 +214,7 @@ const Navbar: React.FC<NavbarProps> = ({ onSearch, onCategorySelect }) => {
           onClick={() => setIsCategoriesOpen((prev) => !prev)}
           className="py-2 px-5 rounded-lg bg-gray-100 text-gray-900 font-semibold shadow-md transition-transform duration-200 ease-in-out hover:bg-green-500 hover:text-white hover:scale-105 flex items-center"
         >
-          📚 <span className="ml-2">Categories</span>
+          📚 <span className="ml-2">All</span>
           <AiOutlineDown
             className={`ml-2 transform transition-transform duration-200 ${isCategoriesOpen ? 'rotate-180' : 'rotate-0'}`}
           />
